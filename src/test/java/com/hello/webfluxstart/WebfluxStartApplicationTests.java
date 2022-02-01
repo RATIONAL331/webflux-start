21c9dcac816593f6f426c231c504f545d74c50d7
RATIONAL331 <rational331@gmail.com>
2022-02-01 16:45:50 +0900

    리포지토리단 테스트, 통합 테스트 추가
    Signed-off-by: 김령진/아이엠스쿨팀/ED <ryungjin.kim@nhnedu.com>

--- src/test/java/com/hello/webfluxstart/WebfluxStartApplicationTests.java
+++ src/test/java/com/hello/webfluxstart/WebfluxStartApplicationTests.java
@@ -1,13 +1,33 @@
 package com.hello.webfluxstart;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 class WebfluxStartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void test() {
+        webTestClient.get().uri("/").exchange()
+                     .expectStatus().isOk()
+                     .expectHeader().contentType(MediaType.TEXT_HTML)
+                     .expectBody(String.class)
+                     .consumeWith(stringEntityExchangeResult -> Assertions.assertThat(stringEntityExchangeResult.getResponseBody()).contains("action=\"/add/"));
+    }
 
 }
--- src/test/java/com/hello/webfluxstart/repository/ItemRepositoryTest.java
+++ src/test/java/com/hello/webfluxstart/repository/ItemRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.hello.webfluxstart.repository;
 
 import com.hello.webfluxstart.model.Item;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.TestPropertySource;
+import reactor.test.StepVerifier;
 
 @DataMongoTest
 // https://stackoverflow.com/questions/70047380/enableautoconfigurationexclude-on-tests-failed-in-spring-boot-2-6-0
@@ -17,5 +19,20 @@ public class ItemRepositoryTest {
     @Test
     public void 하나를_삽입하기() {
         repository.save(new Item("Alf alarm clock", 19.99)).subscribe();
+    }
+
+    @Test
+    public void 하나를_삽입하고_확인하기() {
+        Item sampleItem = new Item("name", "description", 1.99);
+        repository.save(sampleItem)
+                  .as(StepVerifier::create)
+                  .expectNextMatches(item -> {
+                      Assertions.assertThat(item.getId()).isNotNull();
+                      Assertions.assertThat(item.getName()).isEqualTo("name");
+                      Assertions.assertThat(item.getDescription()).isEqualTo("description");
+                      Assertions.assertThat(item.getPrice()).isEqualTo(1.99);
+                      return true;
+                  })
+                  .verifyComplete();
     }
 }
